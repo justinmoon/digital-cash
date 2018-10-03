b065c0973b2914f673293bcf959278feffb04967
Justin Moen <jamoen7@gmail.com>
2018-10-03 13:21:56 -0500

    First pass at BankCoin code

---
+++ bankcoin.py
@@ -0,0 +1,123 @@
+"""
+* comparisons are a PITA here. so absurd that VerifyingKey.to_string() must be called
+* sucks that all my classes need __eq__ methods
+"""
+import uuid
+from copy import deepcopy
+from ecdsa import SigningKey, SECP256k1
+from utils import serialize
+
+
+def transfer_message(previous_signature, public_key):
+    return serialize({
+        "previous_signature": previous_signature,
+        "next_owner_public_key": public_key,
+    })
+
+
+class Transfer:
+    
+    def __init__(self, signature, public_key):
+        self.signature = signature
+        self.public_key = public_key
+
+    def __eq__(self, other):
+        return self.signature == other.signature and \
+               self.public_key.to_string() == other.public_key.to_string()
+
+    def __repr__(self):
+        return f"Transfer(signature={self.signature}, public_key_bytes={self.public_key.to_string()}"
+
+class ECDSACoin:
+
+    def __init__(self, transfers):
+        self.id = uuid.uuid1()
+        self.transfers = transfers
+
+    def __eq__(self, other):
+        return str(self.id) == str(other.id) and self.transfers == other.transfers
+
+    def __repr__(self):
+        return f"ECDSACoin(transfers={self.transfers})"
+
+    @property
+    def last_transfer(self):
+        return self.transfers[-1]
+
+
+    def transfer(self, owner_private_key, recipient_public_key):
+        """Appends to self.transfers"""
+        message = transfer_message(self.last_transfer.signature, recipient_public_key)
+        transfer = Transfer(
+            signature=owner_private_key.sign(message),
+            public_key=recipient_public_key,
+        )
+        self.transfers.append(transfer)
+
+
+class Bank:
+
+    def __init__(self, private_key=None):
+        if not private_key:
+            private_key = SigningKey.generate(curve=SECP256k1)
+        self.private_key = private_key
+        self.public_key = private_key.get_verifying_key()
+
+        # This is our single source of truth
+        self.coins = {}
+
+    def issue(self, public_key):
+        # Create a message specifying who the coin is being issued to
+        message = serialize(public_key)
+        
+        # Create the first transfer, signing with the banks private key
+        signature = self.private_key.sign(message)
+        transfer = Transfer(
+            signature=signature,
+            public_key=public_key,
+        )
+        
+        # Create and return the coin with just the issuing transfer
+        coin = ECDSACoin(transfers=[transfer])
+
+        # Put it in out database of coins
+        self.coins[coin.id] = deepcopy(coin)
+        return coin
+
+    def observe_coin(self, coin):
+        # Look up our copy of the coin
+        last_observation = self.coins[coin.id]
+
+        # New observation of this coin builds off what we know
+        assert last_observation.transfers == \
+               coin.transfers[:len(last_observation.transfers)]
+
+        # Check that transfer history is good
+        self.validate_transfers(coin)
+
+        # Update bank database.
+        self.coins[coin.id] = deepcopy(coin)
+
+    def fetch_coins(self, public_key):
+        coins = []
+        for coin in self.coins.values():
+            if coin.last_transfer.public_key.to_string() == public_key.to_string():
+                coins.append(coin)
+        return coins
+
+    def validate_transfers(self, coin):
+        # Check the first transfer
+        transfer = coin.transfers[0]
+        message = serialize(transfer.public_key)
+        assert self.public_key.verify(transfer.signature, message)
+
+        # Check the subsequent transfers
+        previous_transfer = coin.transfers[0]
+        for transfer in coin.transfers[1:]:
+            # Check previous owner signed this transfer using their private key
+            assert previous_transfer.public_key.verify(
+                transfer.signature,
+                transfer_message(previous_transfer.signature, transfer.public_key),
+            )
+            # Next loop we treat transfer as previous_transfer
+            previous_transfer = transfer
---
+++ bankcoin_tests.py
@@ -0,0 +1,59 @@
+import pytest
+from ecdsa import SigningKey, SECP256k1
+import bankcoin
+from bankcoin import Transfer, Bank, ECDSACoin, transfer_message
+from utils import serialize
+
+# The usual suspects
+bob_private_key = SigningKey.generate(curve=SECP256k1)
+alice_private_key = SigningKey.generate(curve=SECP256k1)
+bob_public_key = bob_private_key.get_verifying_key()
+alice_public_key = alice_private_key.get_verifying_key()
+
+def test_copy():
+    bank = Bank()
+    from copy import deepcopy
+    coin = bank.issue(alice_public_key)
+    copied_coin = deepcopy(coin)
+    assert coin == copied_coin
+    assert coin is not copied_coin
+    coin.transfers.append("puppy")
+    assert len(coin.transfers) != len(copied_coin.transfers)
+
+def test_valid_transfers():
+    # Issue a coin and transfer it twice
+
+    # Bank issues coins to Alice
+    bank = Bank()
+    coin = bank.issue(alice_public_key)
+    bank.validate_transfers(coin)
+
+    assert bank.fetch_coins(alice_public_key) == [coin]
+    assert bank.fetch_coins(bob_public_key) == []
+
+    # Alice sends to Bob
+    coin.transfer(
+        owner_private_key=alice_private_key,
+        recipient_public_key=bob_public_key,
+    )
+    bank.validate_transfers(coin)
+
+    assert len(bank.fetch_coins(alice_public_key)) == 1
+    assert len(bank.fetch_coins(bob_public_key)) == 0
+
+    # Update bank's observation and check balances
+    bank.observe_coin(coin)
+    assert bank.fetch_coins(alice_public_key) == []
+    assert bank.fetch_coins(bob_public_key) == [coin]
+
+    # Bob sends to Bank
+    coin.transfer(
+        owner_private_key=bob_private_key,
+        recipient_public_key=bank.public_key,
+    )
+    bank.validate_transfers(coin)
+
+    # Update bank's observation and check balances
+    bank.observe_coin(coin)
+    assert bank.fetch_coins(alice_public_key) == []
+    assert bank.fetch_coins(bob_public_key) == []
