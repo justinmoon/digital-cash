76ac1051c7ce8a9ed7c592258a5b5c0b429e6485
Justin Moen <jamoen7@gmail.com>
2018-10-14 16:32:12 -0500

    Add public key comparison unittest

--- banknetcoin_tests.py
+++ banknetcoin_tests.py
@@ -1,8 +1,8 @@
 import uuid, pytest
-from ecdsa import SigningKey, SECP256k1
+from ecdsa import SigningKey, VerifyingKey, SECP256k1
 from ecdsa.keys import BadSignatureError
-# from bankutxocoin import TxIn, TxOut, Tx, Bank
-from banknetcoin import TxIn, TxOut, Tx, Bank
+from bankutxocoin import TxIn, TxOut, Tx, Bank
+# from mybanknetcoin import TxIn, TxOut, Tx, Bank
 
 # The usual suspects
 bob_private_key = SigningKey.generate(curve=SECP256k1)
@@ -34,3 +34,14 @@ def test_bank_balances():
 
     with pytest.raises(BadSignatureError):
         bank.handle_tx(alice_to_bob)
+
+
+def test_public_key_comparisons():
+    derived_bob_public_key = VerifyingKey.from_string(
+        bob_public_key.to_string(),
+        curve=SECP256k1,
+    )
+
+    assert bob_public_key.to_string() == derived_bob_public_key.to_string()
+
+    assert bob_public_key == derived_bob_public_key
