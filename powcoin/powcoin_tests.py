df335d2b8665c94b2ab92add35e2e717f4039183
Justin Moen <jamoen7@gmail.com>
2018-11-27 00:42:08 -0600

    POWCoin tests ...

--- powcoin/powcoin_tests.py
+++ powcoin/powcoin_tests.py
@@ -1,105 +1,152 @@
-import pytest, time, uuid, ecdsa
-import identities
-from blockcoin import *
+from copy import deepcopy
+import powcoin as p
+import identities as ids
 
+###########
+# Helpers #
+###########
 
-def test_blocks():
-    bank = Bank(id=0, private_key=identities.bank_private_key(0))
+# Set difficuly very low
+p.POW_TARGET = 2** (256 - 2)
 
-    # Good block
-    block = Block(txns=[])
-    block.sign(bank.private_key)
-    bank.handle_block(block)
-    assert len(bank.blocks) == 1
+def send_tx(node, sender_private_key, recipient_public_key, amount):
+    utxos = node.fetch_utxos(sender_private_key.get_verifying_key())
+    return p.prepare_simple_tx(utxos, sender_private_key, recipient_public_key, amount)
 
-    # Wrong bank signs
-    block = Block(txns=[])
-    wrong_private_key = identities.bank_private_key(1000) 
-    block.sign(wrong_private_key)
-    with pytest.raises(ecdsa.keys.BadSignatureError):
-        bank.handle_block(block)
-
-def test_bad_tx():
-    bank = Bank(id=0, private_key=identities.bank_private_key(0))
-    tx = identities.airdrop_tx()
-    bank.airdrop(tx)
-
-    tx = prepare_simple_tx(
-        utxos=bank.fetch_utxos(identities.alice_public_key),
-        sender_private_key=identities.alice_private_key,
-        recipient_public_key=identities.bob_public_key,
-        amount=10,
+def mine_block(node, miner_public_key, prev_block, mempool, height):
+    coinbase = p.prepare_coinbase(miner_public_key, height)
+    unmined_block = p.Block(
+        txns=[coinbase] + deepcopy(mempool),
+        prev_id=prev_block.id,
     )
-    # Put in a phony signature
-    tx.tx_ins[0].signature = identities.alice_private_key.sign(b"bad")
+    mined_block = p.mine_block(unmined_block)
+    node.handle_block(mined_block)
+    return mined_block
 
-    with pytest.raises(ecdsa.keys.BadSignatureError):
-        bank.handle_tx(tx)
+def new_node():
+    node = p.Node(address="")
 
-def test_airdrop():
-    bank = Bank(id=0, private_key=identities.bank_private_key(0))
-    tx = identities.airdrop_tx()
-    bank.airdrop(tx)
+    # Bob mines height=0
+    p.mine_genesis_block(node, ids.bob_public_key)
 
-    assert 500_000 == bank.fetch_balance(identities.alice_public_key)
-    assert 500_000 == bank.fetch_balance(identities.bob_public_key)
+    # Alice mines height=1
+    mine_block(node, ids.alice_public_key, node.chain[-1], [], 1)
+    
+    bob_balance = node.fetch_balance(ids.bob_public_key)
+    alice_balance = node.fetch_balance(ids.alice_public_key)
+    print(bob_balance, alice_balance)
 
+    # Bob mines height=2,3,4 including one bob-to-alice txn
+    mine_block(node, ids.bob_public_key, node.chain[-1], [], 2)
+    bob_to_alice = send_tx(node, ids.bob_private_key, 
+                           ids.alice_public_key, 10)
+    mine_block(node, ids.bob_public_key, node.chain[-1], [bob_to_alice], 3)
+    mine_block(node, ids.bob_public_key, node.chain[-1], [], 4)
 
-def test_utxo():
-    bank = Bank(id=0, private_key=identities.bank_private_key(0))
-    tx = identities.airdrop_tx()
-    bank.airdrop(tx)
-    assert len(bank.blocks) == 1
+    # Alice forks off 2 blocks at height=1 including one alice-to-bob txn
+    mine_block(node, ids.alice_public_key, node.chain[1], [], 2)
+    alice_to_bob = send_tx(node, ids.alice_private_key, 
+                           ids.bob_public_key, 10)
+    mine_block(node, ids.alice_public_key, node.branches[0][-1], 
+               [alice_to_bob], 3)
 
-    # Alice sends 10 to Bob
-    tx = prepare_simple_tx(
-        utxos=bank.fetch_utxos(identities.alice_public_key),
-        sender_private_key=identities.alice_private_key,
-        recipient_public_key=identities.bob_public_key,
-        amount=10
-    )
-    block = Block(txns=[tx])
-    block.sign(identities.bank_private_key(1))
-    bank.handle_block(block)
-
-    assert 500_000 - 10 == bank.fetch_balance(identities.alice_public_key)
-    assert 500_000 + 10 == bank.fetch_balance(identities.bob_public_key)
+    return node
 
 
-# These three tests cover the 3 cases described in #15 https://en.bitcoin.it/wiki/Protocol_rules#.22block.22_messages 
-def test_extend_active_chain():
-    # Same check as check_blocks above
+def make_node():
+    node = p.Node(address="")
+
+    # Bob mines height=0
+    p.mine_genesis_block(node, ids.bob_public_key)
+
+    return node
+
+#########
+# Tests #
+#########
+
+def test_extend_chain():
+    node = make_node()
+
+    # Bob mines height=1
+    block = mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
+    
+    # Alice's balance unchanged, Bob received block subsidy
+    assert node.fetch_balance(ids.alice_public_key) == 0 
+    assert node.fetch_balance(ids.bob_public_key) == 2*p.BLOCK_SUBSIDY
+
+    # Chain extended
+    assert len(node.chain) == 2
+    assert node.chain[-1] == block
+
+    # Branches empty
+    assert node.branches == []
+
+def test_fork_chain():
+    node = make_node()
+
+    # Bob mines height=1
+    bob_block = mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
+
+    # Alice mines height=1 too
+    alice_block = mine_block(node, ids.alice_public_key, node.chain[0], [], 1)
+
+    # UTXO database unchanged
+    assert node.fetch_balance(ids.alice_public_key) == 0 
+    assert node.fetch_balance(ids.bob_public_key) == 2*p.BLOCK_SUBSIDY
+
+    # Chain unchanged
+    assert len(node.chain) == 2
+    assert alice_block not in node.chain
+
+    # One more chain with one block on it
+    assert len(node.branches) == 1
+    assert node.branches[0] == [alice_block]
+
+def test_block_extending_fork():
+    # Mine one block atop Alice's last block
+
+    # UTXOs and chain unaffected
+
+    # Now new branches'
+
+    # Alice's branch extended by one
+
     pass
 
-def test_extend_side_chain():
-    # same as check_blocks -- but active_chain doesn't change
+def test_block_forking_fork():
+    # Mine block atop Alice's first fork block
+     
+    # UTXOs and chain unaffected
+
+    # One more branch added, which contains alice's first block and this one
+
     pass
 
-def test_change_active_chain():
-    # 2 chains with different amounts of work
+def test_successful_reorg():
+    # Create and handle two block atop Alice's chain
 
-    # call node.handle_block() on a block that changes the balance
+    # Check that utxo from bob's blocks 2,3,4 get removed
 
-    # Check that bank.active_chain and bank.active_chain_index both changed
+    # Check that utxo from alice's blocks 2,3,4,5 get added
 
-    # Transaction contained in old longest chain but not in new longest chain 
-    # 1. Back in mempool
-    # 2. Removed from utxo
+    # Check that txs from blocks 2,3,4 get added back to mempool
 
-    # Check that transaction that was in both chains
-    # 1. Not in mempool
-    # 2. In utxo
+    # Check that coinbases don't get added back to mempool
+
+    # Check that bobs 2,3,4 are now preserved as a branch
+
+    # Check that old branch was removed
+
     pass
 
-def test_chainwork():
-    pass
+def test_unsuccessful_reorg():
+    # Create one valid block for Alice
 
-def test_mining():
-    # use a ridiculously low difficulty?
-    pass
+    # Create one invalid block for Alice
 
-def test_tx_validation():
-    # test a coinbase
-    # node.validate_tx(tx, as_coinbase=True)
-    pass
+    # Exception raised
 
+    # UTXO, chain, branches unchanged
+
+    pass
