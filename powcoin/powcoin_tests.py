7ca49be0812be9c18b3a99aed99db1a67b6bc115
Justin Moen <jamoen7@gmail.com>
2018-11-27 19:34:53 -0600

    Reorg test

--- powcoin/powcoin_tests.py
+++ powcoin/powcoin_tests.py
@@ -13,11 +13,12 @@ def send_tx(node, sender_private_key, recipient_public_key, amount):
     utxos = node.fetch_utxos(sender_private_key.get_verifying_key())
     return p.prepare_simple_tx(utxos, sender_private_key, recipient_public_key, amount)
 
-def mine_block(node, miner_public_key, prev_block, mempool, height):
+def mine_block(node, miner_public_key, prev_block, mempool, height, nonce=0):
     coinbase = p.prepare_coinbase(miner_public_key, height)
     unmined_block = p.Block(
         txns=[coinbase] + deepcopy(mempool),
         prev_id=prev_block.id,
+        nonce=nonce,
     )
     mined_block = p.mine_block(unmined_block)
     node.handle_block(mined_block)
@@ -66,9 +67,10 @@ def make_node():
 #########
 
 def test_extend_chain():
-    node = make_node()
+    node = p.Node(address="")
 
-    # Bob mines height=1
+    # Bob mines height=0,1
+    p.mine_genesis_block(node, ids.bob_public_key)
     block = mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
     
     # Alice's balance unchanged, Bob received block subsidy
@@ -84,8 +86,10 @@ def test_extend_chain():
 
 def test_fork_chain():
     node = make_node()
+    node = p.Node(address="")
 
-    # Bob mines height=1
+    # Bob mines height=0,1
+    p.mine_genesis_block(node, ids.bob_public_key)
     bob_block = mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
 
     # Alice mines height=1 too
@@ -104,41 +108,113 @@ def test_fork_chain():
     assert node.branches[0] == [alice_block]
 
 def test_block_extending_fork():
-    # Mine one block atop Alice's last block
+    node = p.Node(address="")
 
-    # UTXOs and chain unaffected
+    # Bob mines height=0,1,2
+    p.mine_genesis_block(node, ids.bob_public_key)
+    mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
+    bob_block = mine_block(node, ids.bob_public_key, node.chain[1], [], 2)
 
-    # Now new branches'
+    # Alice mines height=1
+    alice_block = mine_block(node, ids.alice_public_key, node.chain[0], [], 1)
+    # Alice mines block on top of her branch
+    alice_block = mine_block(node, ids.alice_public_key, node.branches[0][0], [], 2)
 
-    # Alice's branch extended by one
+    # UTXOs
+    assert node.fetch_balance(ids.alice_public_key) == 0 
+    assert node.fetch_balance(ids.bob_public_key) == 3*p.BLOCK_SUBSIDY
 
-    pass
+    # Now new branches
+    assert len(node.chain) == 3
+    assert len(node.branches) == 1
+    assert len(node.branches[0]) == 2
 
 def test_block_forking_fork():
-    # Mine block atop Alice's first fork block
+    node = p.Node(address="")
+
+    # Bob mines height=0,1,2
+    p.mine_genesis_block(node, ids.bob_public_key)
+    mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
+    bob_block = mine_block(node, ids.bob_public_key, node.chain[1], [], 2)
+
+    # Alice mines height=1
+    first = mine_block(node, ids.alice_public_key, node.chain[0], [], 1)
+
+    # Alice mines 2 separate blocks top of her branch, each at height 2
+    second = mine_block(node, ids.alice_public_key, node.branches[0][0], [], 2)
+    third = mine_block(node, ids.alice_public_key, node.branches[0][0], [], 
+                       2, nonce=second.nonce+1)
      
     # UTXOs and chain unaffected
+    assert node.fetch_balance(ids.alice_public_key) == 0 
+    assert node.fetch_balance(ids.bob_public_key) == 3*p.BLOCK_SUBSIDY
 
     # One more branch added, which contains alice's first block and this one
-
-    pass
+    assert len(node.chain) == 3
+    assert len(node.branches) == 2
+    assert node.branches[0] == [first, second]
+    assert node.branches[1] == [first, third]
 
 def test_successful_reorg():
-    # Create and handle two block atop Alice's chain
+    node = p.Node(address="")
+    alice_node = p.Node(address="")
 
-    # Check that utxo from bob's blocks 2,3,4 get removed
+    # Bob mines height=0,1,2
+    b0 = p.mine_genesis_block(node, ids.bob_public_key)
+    b1 = mine_block(node, ids.bob_public_key, node.chain[0], [], 1)
+    # height=2 contains a bob->alice txn
+    bob_to_alice = send_tx(node, ids.bob_private_key, 
+                           ids.alice_public_key, 10)
+    b2 = mine_block(node, ids.bob_public_key, node.chain[1], 
+                    [bob_to_alice], 2)
 
-    # Check that utxo from alice's blocks 2,3,4,5 get added
+    # Alice accepts bob's first two blocks, but not the third
+    p.mine_genesis_block(alice_node, ids.bob_public_key) # FIXME confusing
+    assert len(alice_node.chain) == 1
+    alice_node.handle_block(b1)
+    assert len(alice_node.chain) == 2
 
-    # Check that txs from blocks 2,3,4 get added back to mempool
+    # FIXME just borrow everything up until this point from another test
+    # Create and handle two blocks atop Alice's chain
+    a2 = mine_block(node, ids.alice_public_key, node.chain[1], [], 1)
+    alice_node.handle_block(a2)
 
-    # Check that coinbases don't get added back to mempool
+    # Chains
+    assert len(node.chain) == 3
+    assert node.chain == [b0, b1, b2]
+    assert len(node.branches) == 1
+    assert node.branches[0] == [a2]
 
-    # Check that bobs 2,3,4 are now preserved as a branch
+    # Balances
+    assert (bob_to_alice.id, 0) in node.utxo_set
+    assert (bob_to_alice.id, 1) in node.utxo_set
+    assert node.fetch_balance(ids.alice_public_key) == 10
+    assert node.fetch_balance(ids.bob_public_key) == 3*p.BLOCK_SUBSIDY - 10
 
-    # Check that old branch was removed
+    # Use alice's node to assemble this txn b/c she doesn't have any utxos in bob's view of world
+    alice_to_bob = send_tx(alice_node, ids.alice_private_key, 
+                           ids.bob_public_key, 20)
+    a3 = mine_block(node, ids.alice_public_key, node.branches[0][0], 
+                    [alice_to_bob], 2)
 
-    pass
+
+    # Chains
+    assert len(node.chain) == 4
+    assert node.chain == [b0, b1, a2, a3]
+    assert len(node.branches) == 1
+    assert node.branches[0] == [b2]
+
+    # Balances
+    assert (bob_to_alice.id, 0) not in node.utxo_set
+    assert (bob_to_alice.id, 1) not in node.utxo_set
+    assert (alice_to_bob.id, 0) in node.utxo_set
+    assert (alice_to_bob.id, 1) in node.utxo_set
+    assert node.fetch_balance(ids.alice_public_key) == 2*p.BLOCK_SUBSIDY - 20
+    assert node.fetch_balance(ids.bob_public_key) == 2*p.BLOCK_SUBSIDY + 20
+
+    # Mempool
+    assert len(node.mempool) == 1
+    assert bob_to_alice in node.mempool
 
 def test_unsuccessful_reorg():
     # Create one valid block for Alice
